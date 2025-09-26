9cd3a0cb93a5fc2ddd73f089a8af897f7f7b5000
Tommy Danks <tommydanks2000@outlook.com>
2025-09-26 05:01:37 +0100

    feat(conference-filter): add options group label for improved clarity in selection

--- src/features/games/components/ConferenceFilterClient.tsx
+++ src/features/games/components/ConferenceFilterClient.tsx
@@ -48,6 +48,7 @@ export default function ConferenceFilterClient() {
 				selected={selectedConferences.map(String)}
 				onChange={(vals) => onConferenceChange(vals.map(Number))}
 				placeholder="Filter by Conference"
+				optionsGroupLabel="Conferences"
 			/>
 		</div>
 	);
